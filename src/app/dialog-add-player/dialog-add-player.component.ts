92c1358abe875b1ad87f7559e1681670449efd22
ThomasJilge <thomasjilge@gmx.de>
2024-08-18 10:38:14 +0200

    dialog added

---
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-dialog-add-player',
+  standalone: true,
+  imports: [
+    MatDialogModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatButtonModule, 
+    FormsModule
+  ],
+  templateUrl: './dialog-add-player.component.html',
+  styleUrl: './dialog-add-player.component.scss'
+})
+export class DialogAddPlayerComponent {
+  name: string = '';
+
+  onNoClick() {
+    
+  }
+
+}
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -4,11 +4,14 @@ import { Game } from '../../models/game';
 import { PlayerComponent } from "../player/player.component";
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule],
+  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule, MatDialogModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
@@ -16,6 +19,10 @@ export class GameComponent implements OnInit {
   pickCardAnimation = false;
   currentCard: string = '';
   game: Game = new Game;
+
+  constructor(public dialog: MatDialog) {
+
+  }
 
   ngOnInit(): void {
     this.newGame();
@@ -42,4 +49,13 @@ export class GameComponent implements OnInit {
   }
 }
 
+openDialog(): void {
+  const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+  dialogRef.afterClosed().subscribe(result => {
+    console.log('The dialog was closed');
+    }
+  )};
 }
+
+
