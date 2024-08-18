aa9b3b6fa9331c00a1abf184941c6645e24b0f8b
ThomasJilge <thomasjilge@gmx.de>
2024-08-18 21:19:18 +0200

    show next player

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -49,7 +49,9 @@ export class GameComponent implements OnInit {
     this.pickCardAnimation = true;
     console.log('New Card: ' + this.currentCard);
     console.log('Game is', this.game);
-    
+
+    this.game.currentPlayer++;
+    this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
     setTimeout(() => {
       this.game.playedCards.push(this.currentCard);
       this.pickCardAnimation = false;
